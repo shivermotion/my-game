0d66a6fdd60c1e812feb885634aa8d6f7419bea6
Jason Day <jason.day.blue@gmail.com>
2023-07-31 00:31:47 -0500

    menu button added

---
+++ src/components/navbars/Private/Private.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAddressCard, faCalendarAlt, faCircleHalfStroke, faGear, faPerson, faTachometerAlt } from '@fortawesome/free-solid-svg-icons';
+
+const PrivateNavBar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className={`fixed top-0 left-0 bg-black w-24 h-24 rounded-br-full transition-all duration-1000 ease-in-out z-10 ${isOpen ? 'w-72 h-72' : ''}`}>
+      <div className="absolute top-0 left-0 w-12 h-12 rounded-full cursor-pointer bg-white z-20 transition-all duration-1000 ease-in-out"
+           onClick={() => setIsOpen(!isOpen)}>
+        <div className={`absolute w-6 h-1 bg-gray-800 rounded-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${isOpen ? 'opacity-0' : ''}`}></div>
+        <div className={`absolute w-6 h-1 bg-gray-800 rounded-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${isOpen ? 'rotate-45' : 'translate-y-[-10px]'}`}></div>
+        <div className={`absolute w-6 h-1 bg-gray-800 rounded-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${isOpen ? 'rotate-[-45deg]' : 'translate-y-[10px]'}`}></div>
+      </div>
+      {isOpen && (
+        <div className="absolute top-0 left-0 z-10 flex flex-col space-y-4 p-4 h-full w-full">
+          <button><FontAwesomeIcon icon={faCircleHalfStroke} className="text-white absolute transform rotate-45 top-[10%] right-[10%] text-6xl" /></button>
+          <button><FontAwesomeIcon icon={faAddressCard} className="text-white absolute top-[40%] left-[40%] text-6xl" /></button>
+          <button><FontAwesomeIcon icon={faGear} className="text-white absolute transform -rotate-45 top-[65%] left-[5%] text-6xl" /></button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PrivateNavBar;
--- src/root-navigation/index.tsx
+++ src/root-navigation/index.tsx
@@ -8,20 +8,26 @@ import { Profile } from "../scenes/Profile";
 import { CapsuleMachineSelection } from "../scenes/CapsuleMachineSelection/CapsuleMachineSelection";
 import { MiniGameSelection } from "../scenes/MiniGameSelection";
 import { Intro } from "../scenes/IntroVideo/IntroVideo";
+import { PrivateLayout } from "../components/layouts/Private/Private";
 import "../globals/globals.css"
+import 'font-awesome/css/font-awesome.min.css';
+
 
 
 export const RootNavigation: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/intro" replace />} />
-      <Route path="/intro" element={<Intro />} /> {/* Add this line */}
+      <Route path="/intro" element={<Intro />} /> 
+      {/* //PrivateLayout */}
+      <Route path="/" element={<PrivateLayout />} >
       <Route path="/main-menu" element={<MainMenu />} />
       <Route path="/options" element={<OptionMenu />} />
       <Route path="/collection" element={<Collection />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/capsule" element={<CapsuleMachineSelection />} />
       <Route path="/minigames" element={<MiniGameSelection />} />
+      </Route>
       <Route path="*" element={<NotFound />} /> {/* 404 page */}
     </Routes>
   );
