f75881f26e2942e514f62ac7e620b7757a8390c5
Jason Day <jason.day.blue@gmail.com>
2023-08-06 21:06:00 -0500

    Save

--- src/components/ui/special/Carousel/Carousel.tsx
+++ src/components/ui/special/Carousel/Carousel.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import AnimatedHand from '../AnimatedHand/AnimatedHand';
 
 interface CarouselProps {
   imageList: string[];
@@ -24,20 +25,22 @@ export const Carousel: React.FC<CarouselProps> = ({imageList, textList}) => {
 
   return (
     <div className='w-full h-full flex '>
-      <div className='w-3/4 h-full'>
+      <div className='w-3/4 h-full m-4 '>
         <Slider {...settings}>
           {imageList.map((imageUrl, index) => (
-            <div key={index} className="h-[80vh] w-full flex items-center justify-center overflow-hidden">
-              <img className="w-full h-full object-cover" src={imageUrl} alt={`carousel ${index + 1}`} />
+            <div key={index} className="h-[80vh] w-full flex items-center justify-center overflow-hidden rounded-3xl hover:cursor-pointer  ">
+              <img className="w-full h-full object-contain" src={imageUrl} alt={`carousel ${index + 1}`} />
             </div>
           ))}
         </Slider>
       </div>
-      <div className='w-1/4 h-grow bg-gray-200 p-4'>
+      <div className='w-1/4 h-grow border-2 border-gray-500 rounded-3xl m-4 bg-opacity-[.5] bg-gray-200 p-4 flex flex-col justify-between'>
         <p className='text-gray-700'>{textList[currentIndex]}</p>
+        <div>
         <p className='text-gray-700 p-4'>Themes:</p>
         <p className='text-gray-700 p-4'>Rarity:</p>
         <p className='text-gray-700 p-4'>Cost:</p>
+        </div>
 
       </div>
     </div>
--- src/scenes/CapsuleMachineSelection/CapsuleMachineSelection.tsx
+++ src/scenes/CapsuleMachineSelection/CapsuleMachineSelection.tsx
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Carousel } from "../../components/ui/special/Carousel/Carousel";
 import { Background } from "../MainMenu/Background/Background";
+import machine1 from '../../assets/images/machine1.png';
+import machine2 from '../../assets/images/machine2.png';
+import machine3 from '../../assets/images/machine2.png';
+import AnimatedHand from "../../components/ui/special/AnimatedHand/AnimatedHand";
+
+
 
 
 
 
 export const CapsuleMachineSelection: React.FC = () => {
-  const imagesList = [
-    "https://i.pinimg.com/originals/0f/6e/0f/0f6e0f0b0b0b0b0b0b0b0b0b0b0b0b0b.jpg",
-    "https://i.pinimg.com/originals/0f/6e/0f/0f6e0f0b0b0b0b0b0b0b0b0b0b0b0b0b.jpg",
-    "https://i.pinimg.com/originals/0f/6e/0f/0f6e0f0b0b0b0b0b0b0b0b0b0b0b0b0b.jpg",
-  ];
-
+  const [showHand, setShowHand] = useState(true);
+  const imagesList = [machine1, machine2, machine3];
   const loremIpsumText = [
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique sollicitudin nibh sit amet consequat. Curabitur vitae consequat dui. Duis elementum orci sed diam consectetur, at volutpat mauris elementum.",
-  
     "Maecenas faucibus urna a dolor blandit, id tincidunt nulla vestibulum. Morbi sed ligula rutrum, bibendum risus a, lacinia orci. Fusce a dui a velit rutrum interdum in a enim.",
-  
     "Praesent at orci cursus, convallis tellus in, congue mi. Donec rutrum nisi sit amet nisl tristique, eu tincidunt mi fermentum. In sed erat eget eros accumsan facilisis."
   ];
+  
+  useEffect(() => {
+    const handleMouseDown = () => {
+      setShowHand(false);
+    }
+ 
+    window.addEventListener("mousedown", handleMouseDown);
+ 
+    return () => {
+      window.removeEventListener("mousedown", handleMouseDown);
+    }
+ }, []);
+ 
   
 
 
@@ -26,9 +39,10 @@ export const CapsuleMachineSelection: React.FC = () => {
   return (
     <>
     <Background/>
-    <div>
-     <h1 className=" justify-center flex shadow-lg p-4 rounded-3xl bg-[#FED7AA] m-8 border-2 border-orange-600   ">Choose Capsule Machine</h1>
-     <Carousel imageList={imagesList} textList={loremIpsumText} />
+    <div className="relative"> {/* Ensures the hand is positioned relative to this container */}
+      <h1 className="justify-center flex shadow-lg p-4 rounded-3xl bg-[#FED7AA] m-8 border-2 border-orange-600">Choose Capsule Machine</h1>
+      <Carousel imageList={imagesList} textList={loremIpsumText} />
+      {showHand && <AnimatedHand />}
     </div>
     </>
   );
