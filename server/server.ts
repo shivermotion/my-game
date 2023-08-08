c114be327453a3c62b4ea477253e9da96c7ee0f0
Jason Day <jason.day.blue@gmail.com>
2023-08-08 03:18:10 -0500

    connected to mongo and server running

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,7 @@ import * as trpcExpress from '@trpc/server/adapters/express';
 import express from 'express';
 import { createServer } from 'http';
 import { MongoClient } from 'mongodb';
-import passport from 'passport';
+import passport, { session } from 'passport';
 import { Strategy as DiscordStrategy } from 'passport-discord';
 import { createContext } from './context/context';
 import { t } from './routers/tRPCRouter';
@@ -14,10 +14,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // MongoDB setup
-const client = new MongoClient('mongodb://localhost:27017');
+const client = new MongoClient(process.env.DB_STRING!);
 
 async function connectToDb() {
+  try {
     await client.connect();
+    console.log("Connected to MongoDB");
+} catch (err) {
+    console.error("Failed to connect to MongoDB", err);
+}
 }
 
 connectToDb();
@@ -30,21 +35,25 @@ passport.use(new DiscordStrategy({
     clientID: process.env.DISCORD_CLIENT_ID!,
     clientSecret: process.env.DISCORD_CLIENT_SECRET!,
     callbackURL: "http://localhost:3000/auth/discord/callback",
-    scope: ['identify', 'guilds']
-}, async (accessToken, refreshToken, profile, done) => {
-    const user = await db.collection('users').findOne({ discordId: profile.id });
-    if (user) {
-        return done(null, user);
-    } else {
-        const newUser = {
-            discordId: profile.id,
-            username: profile.username,
-            discriminator: profile.discriminator,
-            avatar: profile.avatar,
-            guilds: profile.guilds
-        };
-        await db.collection('users').insertOne(newUser);
-        return done(null, newUser);
+    scope: ['identify', 'email']
+  }, async (accessToken, refreshToken, profile, done) => {
+    try {
+        const user = await db.collection('users').findOne({ discordId: profile.id });
+        if (user) {
+            return done(null, user);
+        } else {
+            const newUser = {
+                discordId: profile.id,
+                username: profile.username,
+                discriminator: profile.discriminator,
+                avatar: profile.avatar,
+                guilds: profile.guilds
+            };
+            await db.collection('users').insertOne(newUser);
+            return done(null, newUser);
+        }
+    } catch (err) {
+        return done(err as any);
     }
 }));
 
@@ -73,7 +82,9 @@ app.get('/auth/discord/callback', passport.authenticate('discord', { failureRedi
     res.redirect('/');
 });
 
+
+
 app.use('/api/trpc', trpcExpress.createExpressMiddleware({ router: AppRouter, createContext }));
 
 const server = createServer(app);
-server.listen(3000);
+app.listen(3000, () => console.log('App listening on port 3000!'));
