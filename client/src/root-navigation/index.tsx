a17a96a6c45715e925f8ee9f107d88cce3096994
Jason Day <jason.day.blue@gmail.com>
2023-08-11 23:49:24 -0500

    login portal created

--- client/src/root-navigation/index.tsx
+++ client/src/root-navigation/index.tsx
@@ -8,6 +8,7 @@ import { Profile } from "../scenes/Profile/Profile";
 import { CapsuleMachineSelection } from "../scenes/CapsuleMachineSelection/CapsuleMachineSelection";
 import { MiniGameSelection } from "../scenes/MiniGameSelection/MiniGameSelection";
 import { Intro } from "../scenes/IntroVideo/IntroVideo";
+import { LoginPortal } from "../scenes/LoginPortal/LoginPortal";
 import { PrivateLayout } from "../components/layouts/Private/Private";
 import "../globals/globals.css"
 import 'font-awesome/css/font-awesome.min.css';
@@ -17,6 +18,7 @@ import 'font-awesome/css/font-awesome.min.css';
 export const RootNavigation: React.FC = () => {
   return (
     <Routes>
+      <Route path="/login" element={<LoginPortal/>} />
       <Route path="/" element={<Navigate to="/intro" replace />} />
       <Route path="/intro" element={<Intro />} /> 
       {/* //PrivateLayout */}
