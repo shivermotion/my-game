c5d170b66b1a2d2ceb1b748c59bc4e85704c9549
Jason Day <jason.day.blue@gmail.com>
2023-08-12 02:30:01 -0500

    user can not authenticate

--- client/src/root-navigation/index.tsx
+++ client/src/root-navigation/index.tsx
@@ -8,29 +8,29 @@ import { Profile } from "../scenes/Profile/Profile";
 import { CapsuleMachineSelection } from "../scenes/CapsuleMachineSelection/CapsuleMachineSelection";
 import { MiniGameSelection } from "../scenes/MiniGameSelection/MiniGameSelection";
 import { Intro } from "../scenes/IntroVideo/IntroVideo";
-import { LoginPortal } from "../scenes/LoginPortal/LoginPortal";
 import { PrivateLayout } from "../components/layouts/Private/Private";
 import "../globals/globals.css"
 import 'font-awesome/css/font-awesome.min.css';
+import { LoginPortal } from "../scenes/LoginPortal/LoginPortal";
 
 
 
 export const RootNavigation: React.FC = () => {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPortal/>} />
-      <Route path="/" element={<Navigate to="/intro" replace />} />
-      <Route path="/intro" element={<Intro />} /> 
-      {/* //PrivateLayout */}
-      <Route path="/" element={<PrivateLayout />} >
-      <Route path="/main-menu" element={<MainMenu />} />
-      <Route path="/options" element={<OptionMenu />} />
-      <Route path="/collection" element={<Collection />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/capsule" element={<CapsuleMachineSelection />} />
-      <Route path="/minigames" element={<MiniGameSelection />} />
+      <Route path="/" element={<LoginPortal />} /> {/* Login portal at root path */}
+      <Route path="/intro" element={<Intro />} />
+      {/* PrivateLayout */}
+      <Route path="/" element={<PrivateLayout />}>
+        <Route path="/main-menu" element={<MainMenu />} />
+        <Route path="/options" element={<OptionMenu />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/capsule" element={<CapsuleMachineSelection />} />
+        <Route path="/minigames" element={<MiniGameSelection />} />
       </Route>
       <Route path="*" element={<NotFound />} /> {/* 404 page */}
     </Routes>
   );
 };
+
