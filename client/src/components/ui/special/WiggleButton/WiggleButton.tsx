1ff4a68b942c8acb36baeb4020b71baebf2dc302
Jason Day <jason.day.blue@gmail.com>
2023-08-22 01:20:35 -0500

    model, capsule open animation, various backend

--- client/src/components/ui/special/WiggleButton/WiggleButton.tsx
+++ client/src/components/ui/special/WiggleButton/WiggleButton.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './WiggleButton.css';
 
+interface WiggleButtonProps {
+  text: string;
+  onClick?: (e:any) => void;
+}
 
-export const WiggleButton = ({text} : {text:string}) => {
+
+export const WiggleButton: React.FC<WiggleButtonProps> = ({ text, onClick })=> {
   return (
-<p className="w-1/3 h-12 bg-[#4E6560] text-white  font-bold flex items-center justify-center border-t-4 border-[#E29A5b] animate-[wiggle_10s_linear_infinite] rounded-[0px_0px_4px_4px] origin-[50%_5em] hover:animate-shake">
+<p className="w-1/3 h-12 bg-[#4E6560] text-white  font-bold flex items-center justify-center border-t-4 border-[#E29A5b] animate-[wiggle_10s_linear_infinite] rounded-[0px_0px_4px_4px] origin-[50%_5em] hover:animate-shake"onClick={onClick}>
   {text}
 </p>
   );
