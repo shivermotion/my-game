2bd144771e0bee824de80438f2bbfb0585ee3b09
Jason <jday@nexrage.com>
2023-09-07 14:46:46 -0500

    save

--- client/src/components/ui/special/SpinningRectangle/SpinningRectangle.tsx
+++ client/src/components/ui/special/SpinningRectangle/SpinningRectangle.tsx
@@ -5,14 +5,14 @@ import "./SpinningRectangle.css";
 
 const SpinningRectangle = ({ onClick }: {onClick?: () => void;}) => {
   return (
-    <div className="container" onClick={onClick}>
-    <div className="cuboid">
-      <div className="face frontSide">Front</div>
-      <div className="face backSide">Back</div>
-      <div className="face topSide">Top</div>
-      <div className="face bottomSide">Bottom</div>
-      <div className="face rightSide">Right</div>
-      <div className="face leftSide">Left</div>
+    <div className="container hover:cursor-pointer hover:animate-wiggle" onClick={onClick}>
+    <div className="cuboid animate-rotate-y animate-infinite animate-duration-[10000ms]">
+      <div className="face frontSide"></div>
+      <div className="face backSide"></div>
+      <div className="face topSide"></div>
+      {/* <div className="face bottomSide">Bottom</div> */}
+      <div className="face rightSide"></div>
+      <div className="face leftSide"></div>
     </div>
   </div>
 );
--- client/src/scenes/MiniGameSelection/MiniGameSelection.tsx
+++ client/src/scenes/MiniGameSelection/MiniGameSelection.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import SpinningRectangle from "../../components/ui/special/SpinningRectangle/SpinningRectangle";
 import Game from "./JTEBoxing/JTEBoxing"; // Import the component directly
+import ModelViewer from "../../components/ui/special/ModelViewer/ModelViewer";
 
 
 export const MiniGameSelection: React.FC = () => {
@@ -17,9 +18,8 @@ export const MiniGameSelection: React.FC = () => {
         MiniGameSelection
       </h1>
       {selectedGame === null ? (
-        <div className="flex flex-col items-center justify-center h-screen">
-          <SpinningRectangle onClick={() => setSelectedGame(<Game />)} />
-          <iframe style={{ width:"800px", height:"600px"}} src="http://tridiv.com/app/index.html?dmlldz1mdWxsJmRvYz17InNldHRpbmdzIjp7Im5hbWUiOiJjYXBzdWxlTWFjaGluZSIsImxpZ2h0Ijoibm9uZSIsInNoYWRlIjoiLjMiLCJ0aW50IjoiLjE1IiwiYm9yZGVyIjoiMC4zNCIsImJnIjoiI2ZmZmZmZiIsInNuYXAiOiJvbiIsIm1hdGNoIjowLjUsInpvb20iOiIxMDAifSwic2hhcGVzIjpbeyJ0eXBlIjoiY3Vib2lkIiwieSI6LTUsInoiOjAuNSwidyI6Ni42MjUsImgiOjYuOTEwNzE0Mjg1NzE0Mjg1LCJkIjo2LjM3NSwiY0FsbCI6IiNmYjJlMDEiLCJuYW1lIjoiY3ViLTEifSx7InR5cGUiOiJjdWJvaWQiLCJ4IjoyLjUsInkiOjIuNSwieiI6MywidyI6MS4zNzUsImgiOjguNSwiZCI6MS4yNSwiY0FsbCI6IiNlMWUxZTEiLCJuYW1lIjoiY3ViLTMifSx7InR5cGUiOiJjdWJvaWQiLCJ4IjoyLjUsInkiOjIuNSwieiI6LTIsInciOjEuNSwiaCI6OC4zNzUsImQiOjEuMjUsImNBbGwiOiIjZTFlMWUxIiwibmFtZSI6ImN1Yi00In0seyJ0eXBlIjoiY3Vib2lkIiwieCI6LTIuNSwieSI6Mi41LCJ6IjotMiwidyI6MS41LCJoIjo4LjM3NSwiZCI6MS4yNSwiY0FsbCI6IiNlMWUxZTEiLCJuYW1lIjoiY3ViLTUifSx7InR5cGUiOiJjdWJvaWQiLCJ4IjotMi41LCJ5IjoyLjUsInoiOjMsInciOjEuNSwiaCI6OC4zNzUsImQiOjEuMjUsImNBbGwiOiIjZTFlMWUxIiwibmFtZSI6ImN1Yi02In0seyJ0eXBlIjoiY3Vib2lkIiwieSI6LTQuNSwieiI6Mi41LCJ3IjozLjE0Mjg1NzE0Mjg1NzE0MzIsImgiOjIuNzE0Mjg1NzE0Mjg1NzE1MywiZCI6My43MTQyODU3MTQyODU3MTM1LCJjQWxsIjoiI2UxZTFlMSIsIm5hbWUiOiJjdWItMiJ9LHsidHlwZSI6ImN5bGluZGVyIiwieSI6LTQuNSwieiI6NCwiZGlhbWV0ZXIiOjEuNDI4NTcxNDI4NTcxNDI3LCJoIjoxLjcxNDI4NTcxNDI4NTcxNTMsInJ4IjotOTAsImNBbGwiOiIjZTFlMWUxIiwibmFtZSI6ImN5bC0xIn0seyJ0eXBlIjoiY3Vib2lkIiwieSI6LTE2LCJ6IjowLjUsInciOjYuNjI1LCJoIjoxLjMzOTI4NTcxNDI4NTcwODIsImQiOjYuOTQ2NDI4NTcxNDI4NTcyLCJjQWxsIjoiI2ZiMmUwMSIsIm5hbWUiOiJjdWItOCJ9LHsidHlwZSI6ImN1Ym9pZCIsInkiOi0xMiwieiI6MC41LCJ3Ijo2LjkxMDcxNDI4NTcxNDI4NSwiaCI6Ni4wNTM1NzE0Mjg1NzE0MjgsImQiOjYuMzc1LCJyeiI6LTkwLCJvIjoiMSIsImNBbGwiOiIjOWRlYWY0IiwibmFtZSI6ImN1Yi03In1dfQ==" title="machine"></iframe>
+        <div className="flex flex-col items-center justify-center h-screen ">
+          <SpinningRectangle  onClick={() => setSelectedGame(<Game />)} />
         </div>
       ) : (
         <div>
