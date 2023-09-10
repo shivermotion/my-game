0fe5313c6334f1c2dd5c1b5f61a641d80baf2c24
Jason Day <jason.day.blue@gmail.com>
2023-09-10 01:16:54 -0500

    Save

---
+++ client/src/components/ui/special/CongratsFrame/CongratsFrame.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const CongratsFrame = () => {
+  return (
+    <div className="congrats-frame">
+      <div className="congrats-frame__title">Congratulations!</div>
+      <div className="congrats-frame__subtitle">
+        You have successfully passed the registration
+      </div>
+    </div>
+  );
+};
