568b15abd4c29761348e3e0f66d9829acc5b7990
Jason <jday@nexrage.com>
2023-10-02 15:01:15 -0500

    Save

--- client/src/scenes/MainMenu/MainMenu.tsx
+++ client/src/scenes/MainMenu/MainMenu.tsx
@@ -3,9 +3,9 @@ import { Background } from "./Background/Background";
 import { TitleText } from "./TitleText/TitleText";
 import { Button } from "../../components/ui/common/Button";
 import { Link, useNavigate } from "react-router-dom";
+import moai from '../../assets/models/moai.glb';
 import ModelViewer from "../../components/ui/special/ModelViewer/ModelViewer";
 import earth from "../../assets/models/GlobeCapsuleCompressed.glb";
-import { World } from "../World/World";
 import "./MainMenu.css";
 
 export const MainMenu = () => {
@@ -22,7 +22,9 @@ export const MainMenu = () => {
       setTimeout(() => {
         setIsWipeActive(false);
       }, 3000);
+      console.log("isWipeActive", isWipeActive);
     }
+    console.log("isWipeActive", isWipeActive);
   }, [isWipeActive, navigate]);
 
   const handleModelClick = () => {
@@ -32,7 +34,7 @@ export const MainMenu = () => {
   return (
     <>
       <Background />
-      <TitleText />
+     {!showNewComponent ? <TitleText /> : null} 
       <div className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${showNewComponent ? 'hidden' : 'block'} z-10`}>
         <div onClick={handleModelClick}>
           <ModelViewer  
@@ -42,14 +44,26 @@ export const MainMenu = () => {
             className="model-viewer-animation"  
           />
         </div>
-        <Link to="/capsule"><Button text="START" /></Link>
-        <Link to="/minigames"><Button text="MINI GAMES" /></Link>
-        <Link to="/options"><Button text="OPTIONS"  /></Link>
+        <Link to="/capsule"><Button text="START"/></Link>
+        <Link to="/minigames"><Button text="MINI GAMES"/></Link>
+        <Link to="/options"><Button text="OPTIONS"/></Link>
       </div>
-      <div className={`new-component absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${showNewComponent ? 'block' : 'hidden'} z-10`}>
-        <World/>
+
+      <div className={`${showNewComponent ? 'block' : 'hidden'} `}>
+        <div className="flex h-screen w-screen">
+          <div className="w-1/4 bg-red-500 items-center justify-center">
+            <ModelViewer src={moai} alt={''} />
+          </div>
+          <div className="w-1/2 bg-blue-500 items-center justify-center">
+            <ModelViewer src={moai} alt={''} />
+          </div>
+          <div className="w-1/4 bg-green-500 items-center justify-center">
+            <ModelViewer src={moai} alt={''} />
+          </div>
+        </div>
       </div>
-      <div className={`wipe-transition ${isWipeActive ? 'active' : ''} z-20`}></div>
+
+      <div className={`wipe-transition ${isWipeActive  ? 'active' : ''} z-20`}/>
     </>
   );
 };
